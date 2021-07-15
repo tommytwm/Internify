5bdbf5dfdde3370f7bf2be7b815e916baf9ca8c4
Rene Reid <rdreid01@students.cs.ubc.ca>
2021-07-15 16:47:45 -0600

    handleEdit partially implemented; additional route added to users.js (routes) (Update route)

--- internify/backend/routes/api/users.js
+++ internify/backend/routes/api/users.js
@@ -27,8 +27,6 @@ router.get("/:id", function (req, res, next) {
 //@access   Public
 
 router.post("/", function (req, res, next) {
-  
-
   // flesh out a new user
   var newUser = new UserData({
     name: req.body.name,
@@ -66,6 +64,39 @@ router.delete("/:id", function (req, res, next) {
       console.log(err);
       res.status(404).json({ success: false });
     });
+});
+
+//@route    UPDATE api/update/:id
+//@desc     UPDATE a User document
+//@access   Public (?)
+
+router.post("/update/:id", function (req, res, next) {
+  UserData.findById(req.params.id)
+    .then((user) => {
+      user.name = req.body.name ? req.body.name : user.name;
+      user.title = req.body.title ? req.body.title : user.title;
+      user.handle = req.body.handle ? req.body.handle : user.handle;
+      user.email = req.body.email ? req.body.email : user.email;
+      user.location = req.body.location ? req.body.location : user.location;
+      user.bio = req.body.bio ? req.body.bio : user.bio;
+      user.contactNumber = req.body.contactNumber
+        ? req.body.contactNumber
+        : user.contactNumber;
+      user.website = req.body.website ? req.body.website : user.website;
+      user.linkedIn = req.body.linkedIn ? req.body.linkedIn : user.linkedIn;
+      user.status = req.body.status ? req.body.status : user.status;
+      user.company = req.body.company ? req.body.company : user.company;
+
+      user
+        .save()
+        .then(() => {
+          console.log(user);
+          // do something more here...
+          // perhaps get the profile page to update with this new information?
+        })
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
--- internify/src/components/molecules/EditModal.js
+++ internify/src/components/molecules/EditModal.js
@@ -3,6 +3,7 @@ import { ButtonFilled, ButtonWhite } from "../atoms/index";
 import { Dialog, DialogContent, DialogTitle, Grid } from "@material-ui/core";
 import { TextFieldInput, MultiLineTextField } from "../atoms/index";
 import "./styles/EditModal.css";
+import axios from "axios";
 
 const EditModal = (props) => {
   let toggle = props.toggle;
@@ -38,9 +39,28 @@ const EditModal = (props) => {
     setToggle(false);
   };
 
-  const handleUpdate = () => {
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+
+    const newProfile = {
+      name: profile.name,
+      title: profile.title,
+      handle: profile.handle,
+      email: profile.email,
+      location: profile.location,
+      bio: profile.bio,
+      contactNumber: profile.contactNumber,
+      website: profile.website,
+      linkedIn: profile.linkedIn,
+      status: profile.status,
+      company: profile.company,
+    }
     //TODO: Make POST request to update user information with `profile`
-    console.log(profile);
+    // NOTE: must determine how to access user's authID. 
+    axios.post(`http://localhost:5000/users/update/${user.id}`, newProfile)
+    
+    console.log(newProfile);
   }
 
   return toggle ? (
@@ -54,6 +74,7 @@ const EditModal = (props) => {
               className="edit_profile_name"
               label="Name"
               type="text"
+              value={profile.name}
               placeholder="Tommy Tho"
               onChange={(e) => setProfile({ ...profile, name: e.target.value })}
             />
@@ -64,6 +85,7 @@ const EditModal = (props) => {
               className="edit_profile_handle"
               label="Handle"
               type="text"
+              value={profile.handle}
               placeholder="@tommytho"
               onChange={(e) =>
                 setProfile({ ...profile, handle: e.target.value })
@@ -78,6 +100,7 @@ const EditModal = (props) => {
               className="edit_profile_title"
               label="Job Title"
               type="text"
+              value={profile.title}
               placeholder="Technical Recruiter"
               onChange={(e) =>
                 setProfile({ ...profile, title: e.target.value })
@@ -90,6 +113,7 @@ const EditModal = (props) => {
               className="edit_profile_location"
               label="Company"
               type="text"
+              value={profile.company}
               placeholder="Internify Solutions Inc."
               onChange={(e) =>
                 setProfile({ ...profile, company: e.target.value })
@@ -103,6 +127,7 @@ const EditModal = (props) => {
             className={"edit_profile_bio"}
             label={"Bio"}
             type={"text"}
+            value={profile.bio}
             rows={4}
             defaultValue={profile.bio}
             onChange={(e) => setProfile({ ...profile, bio: e.target.value })}
@@ -115,6 +140,7 @@ const EditModal = (props) => {
               className="edit_profile_email"
               label="Email"
               type="text"
+              value={profile.email}
               placeholder="tommy@internify.com"
               onChange={(e) =>
                 setProfile({ ...profile, email: e.target.value })
@@ -127,6 +153,7 @@ const EditModal = (props) => {
               className="edit_profile_website"
               label="Website"
               type="text"
+              value={profile.website}
               placeholder="tommytho.com"
               onChange={(e) =>
                 setProfile({ ...profile, website: e.target.value })
@@ -141,6 +168,7 @@ const EditModal = (props) => {
               className="edit_profile_linkedin"
               label="LinkedIn"
               type="text"
+              value={profile.linkedIn}
               placeholder="linkedin.com/in/tommythowm"
               onChange={(e) =>
                 setProfile({ ...profile, linkedIn: e.target.value })
@@ -153,6 +181,7 @@ const EditModal = (props) => {
               className="edit_profile_location"
               label="Location"
               type="text"
+              value={profile.location}
               placeholder="Vancouver, BC"
               onChange={(e) =>
                 setProfile({ ...profile, location: e.target.value })
